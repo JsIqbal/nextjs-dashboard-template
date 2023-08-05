e811aebb515f92c5a30889a50824b7f087c08d53
Ahmad Munab <ahmadmunab22@gmail.com>
2023-08-05 14:40:34 +0600

    logo dimention fixed

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -67,7 +67,7 @@ const Sidebar = () => {
     <div className="space-y-4 py-2 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-2 flex flex-col justify-start items-start flex-1">
         <Link href="/dashboard" className="flex w-full items-center justify-center gap-4 mb-16 mt-6">
-          <div className="relative w-14 h-9 ">
+          <div className="relative w-16 h-8 ">
             <Image fill alt="Logo" src="/logo-new.png" />
           </div>
           <h1 className={cn("text-2xl font-bold", montsserrat.className)}>
